b8bda99020beb0de0437072c20a634f216aa0bf2
Igor Meezes <igor.menezes98@gmail.com>
2023-06-09 14:09:15 +0100

    Commit11

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
 
-var anchor = document.querySelector("#menu").querySelectorAll("a");
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the current URL path
+    const currentPath = window.location.pathname;
+  
+    // Find the menu links and add the active class to the corresponding link
+    const menuLinks = document.querySelectorAll('.menu-link');
+    menuLinks.forEach((link) => {
+      // Modify the comparison logic to check if the current path is equal to the link's href attribute
+      if (link.getAttribute('href') === currentPath || link.getAttribute('href') === `.${currentPath}`) {
+        link.classList.add('active');
+      }
+    });
+  });
+  
+
+/* var anchor = document.querySelector("#menu").querySelectorAll("a");
 
 console.log(anchor);
 for(var i = 0; i < 3; i++) {
     if(window.location.href === anchor[i].href) {
         anchor[i].classList.add("active")
     }
-}
+} */
 
 
 
